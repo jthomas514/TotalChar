819c2e6d3c43310e13cd3b691477c3230360a6c9
jthomas514 <jgthomas514@outlook.com>
2022-06-10 07:44:12 -0700

    Beginnings of saving and loading

--- app/src/main/java/com/example/totalclass/Attribut.kt
+++ app/src/main/java/com/example/totalclass/Attribut.kt
@@ -9,10 +9,7 @@ import android.widget.Toast
 import android.widget.Toast.makeText
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.att_fragment.*
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.File
-import java.io.InputStream
+import java.io.*
 import java.nio.Buffer
 import java.util.*
 
@@ -22,6 +19,23 @@ class Attribute(file_name: String):Fragment(R.layout.att_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val load = File(context?.filesDir, charName)
+        val fileContent = load.readText()
+
+        val str_pattern = "Strength\\sModifier\\s\\d+\\s-?\\d+".toRegex()
+        val dex_pattern = "Dexterity\\sModifier\\s\\d+\\s-?\\d+".toRegex()
+        val int_pattern = "Intelligence\\sModifier\\s\\d+-?\\d".toRegex()
+        val wis_pattern = "Wisdom\\sModifier\\s\\d+\\s-?\\d+".toRegex()
+        val con_pattern = "Constitution\\sModifier\\s\\d+\\s-?\\d+".toRegex()
+        val char_pattern = "Charisma\\sModifier\\s\\d+\\s-?\\d+".toRegex()
+
+        var tempMatch = str_pattern.find(fileContent)
+        var tempRange = tempMatch?.range
+        str?.setText(fileContent[tempRange!!.last + 1].toString()) //only going to give one digit even if its multiple digit, so need to fix
+        //need to run a regex twice to load the values
+        //Only to find the first value, then generate the modifier off them
+
 
 
         gen_att.setOnClickListener(){
@@ -37,25 +51,67 @@ class Attribute(file_name: String):Fragment(R.layout.att_fragment) {
             calc_modifier(wis, wis_mod)
             calc_modifier(con, con_mod)
             calc_modifier(cha, cha_mod)
-            Toast.makeText(context, charName, Toast.LENGTH_SHORT).show()
         }
 
         attr_save.setOnClickListener(){
 
             var file = File(context?.filesDir, charName)
-            file.printWriter().use{ out ->
-                out.println("_____Atributes_____")
-                out.println("Strength Modifier "+ str_mod.text.toString())
-                out.println("Dexterity Modifier "+ dex_mod.text.toString())
-                out.println("Intelligence Modifier "+ int_mod.text.toString())
-                out.println("Wisdom Modifier "+ wis_mod.text.toString())
-                out.println("Constitution Modifier "+ con_mod.text.toString())
-                out.println("Charisma Modifier "+ cha_mod.text.toString())
-                out.println()
-                out.println()
-            }
+            var string_replacer = ""
+
             var file_content = file.readText()
             test_area.text = file_content
+
+            if(file_content == "") {
+                file.printWriter().use{ out ->
+                    out.println("_____Attributes_____")
+                    out.println("Strength Modifier "+ str.text.toString() +" "+ str_mod.text.toString())
+                    out.println("Dexterity Modifier "+ dex.text.toString() +" "+ dex_mod.text.toString())
+                    out.println("Intelligence Modifier "+ inte.text.toString() +" "+ int_mod.text.toString())
+                    out.println("Wisdom Modifier "+ wis.text.toString() +" "+ wis_mod.text.toString())
+                    out.println("Constitution Modifier "+ con.text.toString() +" "+ con_mod.text.toString())
+                    out.println("Charisma Modifier "+ cha.text.toString() +" "+ cha_mod.text.toString())
+                    out.println()
+                    out.println()
+                }
+            }
+
+            else{
+                val pattern = "_____Attributes_____".toRegex()
+                val match = pattern.find(file_content)
+
+
+                val index = match?.value
+                val range = match?.range
+
+                if(match != null){
+                    var temp_new = ""
+                    var temp_match = str_pattern.find(file_content)
+
+                    if(temp_match != null){
+                        temp_new = "Strength Modifier "+ str.text.toString() +" "+ str_mod.text.toString()
+                        string_replacer = file_content.replace(str_pattern, temp_new)
+                        file_content = string_replacer
+                        temp_new = "Dexterity Modifier "+ dex.text.toString() +" "+ dex_mod.text.toString()
+                        string_replacer = file_content.replace(dex_pattern, temp_new)
+                        file_content = string_replacer
+                        temp_new = "Intelligence Modifier "+ inte.text.toString() +" "+ int_mod.text.toString()
+                        string_replacer = file_content.replace(int_pattern, temp_new)
+                        file_content = string_replacer
+                        temp_new = "Wisdom Modifier "+ wis.text.toString() +" "+ wis_mod.text.toString()
+                        string_replacer = file_content.replace(wis_pattern, temp_new)
+                        file_content = string_replacer
+                        temp_new = "Constitution Modifier "+ con.text.toString() +" "+ con_mod.text.toString()
+                        string_replacer = file_content.replace(wis_pattern, temp_new)
+                        file_content = string_replacer
+                        temp_new = "Charisma Modifier "+ cha.text.toString() +" "+ cha_mod.text.toString()
+                        string_replacer = file_content.replace(char_pattern, temp_new)
+                        file_content = string_replacer
+                    }
+                    else
+                        Toast.makeText(context, "This is dumb", Toast.LENGTH_SHORT).show()
+                    file.writeText(file_content)
+                }
+            }
 
         }
 
